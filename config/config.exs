52a01a880c782297fe6b451ace4eaba1a2c67e1c
Faissal Elamraoui <amr.faissal@gmail.com>
2019-01-02 20:03:04 +0100

    Structure config properly

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,11 @@
 use Mix.Config
 
-config :crypto_bot, slack_api_token: System.get_env("SLACK_API_TOKEN")
+config :crypto_bot,
+  slack: [
+    api_key: System.get_env("SLACK_API_TOKEN")
+  ],
+  crypto: [
+    provider: CryptoBot.CryptoCompare,
+    base_url: System.get_env("CRYPTO_BASE_URL"),
+    api_key: System.get_env("CRYPTO_API_KEY")
+  ]
